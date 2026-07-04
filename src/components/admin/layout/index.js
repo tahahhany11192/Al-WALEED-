186c27e0a3bf3c477a71f279844798a14c615969
tahahhany11192 <tahah8833@gmail.com>
2026-07-04 22:40:00 +0300

    Update index.js

--- src/components/admin/layout/index.js
+++ src/components/admin/layout/index.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-
 import AdminNavber from "../partials/AdminNavber";
 import AdminSidebar from "../partials/AdminSidebar";
 import AdminFooter from "../partials/AdminFooter";
@@ -8,13 +7,15 @@ const AdminLayout = ({ children }) => {
   return (
     <Fragment>
       <AdminNavber />
-      <section className="flex bg-gray-100">
+
+      <section className="flex bg-gray-100 min-h-screen">
         <AdminSidebar />
-        <div className="w-full md:w-11/12 h-full">
-          {/* All Children pass from here */}
+
+        <main className="w-full md:w-9/12 lg:w-10/12 min-h-screen p-3 sm:p-4 lg:p-6 pb-24 md:pb-6 overflow-x-hidden">
           {children}
-        </div>
+        </main>
       </section>
+
       <AdminFooter />
     </Fragment>
   );
